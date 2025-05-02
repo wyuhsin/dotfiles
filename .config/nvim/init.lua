a89fb163ca42733b015ac1c561ed58dec2c2abee
wyuhsin <messy_things@outlook.com>
2025-05-02 09:55:46 +0800

    feat: update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,13 @@
-require("options")
-require("mapping")
-require("plugins")
-require("lsp")
-require("nvimcmp")
-require("treesitter")
-require("theme")
-require("nvimtree")
-require("telescop")
+require("core.options")
+require("core.keymaps")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git", lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
